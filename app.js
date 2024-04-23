b75dc16fda89eb1c3664c232fc514fe273d4e6a9
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-23 17:11:35 +0600

    Database Connection

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const hpp =require('hpp');
 const cors =require('cors');
 const cookieParser = require('cookie-parser');
 const path = require("path");
+const mongoose=require('mongoose');
 
 
 app.use(cookieParser());
@@ -20,6 +21,18 @@ app.use(express.urlencoded({limit: '50mb'}));
 
 const limiter= rateLimit({windowMs:15*60*1000,max:3000})
 app.use(limiter)
+
+
+// Database Connection
+let URI="mongodb+srv://mohammad:sumu9897@cluster0.tb1lx0x.mongodb.net/CRUD";
+
+let OPTION={user:'mohammad',pass:'sumu9897',autoIndex:true};
+
+mongoose.connect(URI,OPTION).then((res)=>{
+    console.log("DB Success")
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 app.set('etag', false);
