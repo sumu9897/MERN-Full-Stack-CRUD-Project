5b6dda126bc3f29922f1c35e4d7401904fd35f6c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-23 19:07:06 +0600

    API

--- src/controller/CrudController.js
+++ src/controller/CrudController.js
@@ -1,19 +1,63 @@
-exports.Create=async(req,res)=>{
+const ProductModel = require("../model/ProductModel");
 
-    return res.status(200).json
+exports.Create=async(req,res)=>{
+    try{
+        let reqBody=req.body;
+        await ProductModel.create(reqBody);
+        return res.status(200).json({status:"success",message:"Request Completed"});
+    }catch (e) {
+        return res.status(200).json({err:e.toString()})
+    }
 }
+
+
 
 exports.Read=async(req,res)=>{
-
-    return res.status(200).json
+    try{
+        let rows=await ProductModel.find();
+        return res.status(200).json({status:"success",message:"Request Completed",row:rows});
+    }catch (e) {
+        return res.status(200).json({err:e.toString()})
+    }
 }
+
+
+
+exports.ReadByID=async(req,res)=>{
+    try{
+        let {id}=req.params
+        let rows=await ProductModel.find({_id:id});
+        return res.status(200).json({status:"success",message:"Request Completed",row:rows});
+    }catch (e) {
+        return res.status(200).json({err:e.toString()})
+    }
+}
+
+
+
+
 
 exports.Update=async(req,res)=>{
+    try{
 
-    return res.status(200).json
+        // By ID OLD -> NEW-> Compare --> Change Column Name-CurrentValue/NewValue/Date--> Insert
+
+        let {id}=req.params
+        let reqBody=req.body;
+        await ProductModel.updateOne({_id:id},reqBody);
+        return res.status(200).json({status:"success",message:"Request Completed"});
+    }catch (e) {
+        return res.status(200).json({err:e.toString()})
+    }
 }
 
-exports.Delete=async(req,res)=>{
 
-    return res.status(200).json
+exports.Delete=async(req,res)=>{
+    try{
+        let {id}=req.params
+        await ProductModel.deleteOne({_id:id});
+        return res.status(200).json({status:"success",message:"Request Completed"});
+    }catch (e) {
+        return res.status(200).json({err:e.toString()})
+    }
 }
